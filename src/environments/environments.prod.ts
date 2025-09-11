c86691a056e05a7d3dfaf15bbd00f6d5f3db5351
JoseRT23 <joser16torres@gmail.com>
2025-09-10 19:45:07 -0500

    testing frontend environments

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,5 +1,5 @@
 export const environments = {
-    baseUrl: 'http://3.131.160.76:3000/api/v1/',
-    loginUrl: 'http://3.131.160.76:3000/api/v1/auth/login',
-    refreshTokenUrl: 'http://3.131.160.76:3000/api/v1/auth/refresh',
+    baseUrl: 'http://3.131.160.76/api/v1/',
+    loginUrl: 'http://3.131.160.76/api/v1/auth/login',
+    refreshTokenUrl: 'http://3.131.160.76/api/v1/auth/refresh',
 }
