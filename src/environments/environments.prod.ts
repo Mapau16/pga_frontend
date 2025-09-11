a51596c82ff63755a3a4146d0dfdc9881ddf0a4b
Paula Engativa <pdengativa16@gmail.com>
2025-09-10 19:28:20 -0500

    se modifico el environments

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,5 +1,5 @@
 export const environments = {
-    baseUrl: 'http://localhost:3000/api/v1/',
-    loginUrl: 'http://localhost:3000/api/v1/auth/login',
-    refreshTokenUrl: 'http://localhost:3000/api/v1/auth/refresh',
+    baseUrl: 'http://3.131.160.76:3000/api/v1/',
+    loginUrl: 'http://3.131.160.76:3000/api/v1/auth/login',
+    refreshTokenUrl: 'http://3.131.160.76:3000/api/v1/auth/refresh',
 }
